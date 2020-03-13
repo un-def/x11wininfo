c9b78ef2e35379f195766cb933fe62452f1ddbf7
un.def <un.def@ya.ru>
2020-03-13 10:33:31 +0300

    Use _NET_ACTIVE_WINDOW to get focused window
    A new appoatch requires EWMH but fixes issues with
    broken applications such as Spotify.

--- x11wininfo.c
+++ x11wininfo.c
@@ -7,35 +7,23 @@
 #include <xcb/xproto.h>
 
 
-void die(char *message) {
+void
+die(char *message) {
     fprintf(stderr, "ERROR: %s\n", message);
     exit(1);
 }
 
 
-void die_atom_error(char *atom) {
+void
+die_atom_error(char *atom) {
     char error[64];
     sprintf(error, "cannot get %s atom", atom);
     die(error);
 }
 
 
-xcb_window_t get_focused_window(xcb_connection_t *conn) {
-    xcb_window_t focused;
-    xcb_get_input_focus_cookie_t cookie;
-    xcb_get_input_focus_reply_t *reply;
-    cookie = xcb_get_input_focus(conn);
-    reply = xcb_get_input_focus_reply(conn, cookie, NULL);
-    if (!reply) {
-        return 0;
-    }
-    focused = reply->focus;
-    free(reply);
-    return focused;
-}
-
-
-xcb_atom_t get_atom(xcb_connection_t *conn, char *name) {
+xcb_atom_t
+get_atom(xcb_connection_t *conn, char *name) {
     xcb_atom_t atom;
     xcb_intern_atom_cookie_t cookie;
     xcb_intern_atom_reply_t *reply;
@@ -50,7 +38,8 @@ xcb_atom_t get_atom(xcb_connection_t *conn, char *name) {
 }
 
 
-char *get_window_property(xcb_connection_t *conn, xcb_window_t window, char *property, char *type, uint8_t empty_to_null) {
+xcb_get_property_reply_t *
+get_window_property_reply(xcb_connection_t *conn, xcb_window_t window, char *property, char *type) {
     xcb_atom_t property_atom = get_atom(conn, property);
     if (!property_atom) {
         die_atom_error(property);
@@ -66,6 +55,29 @@ char *get_window_property(xcb_connection_t *conn, xcb_window_t window, char *pro
     if (!reply) {
         return NULL;
     }
+    return reply;
+}
+
+
+xcb_window_t
+get_focused_window(xcb_connection_t *conn, xcb_window_t root) {
+    xcb_get_property_reply_t *reply;
+    reply = get_window_property_reply(conn, root, "_NET_ACTIVE_WINDOW", "WINDOW");
+    if (!reply) {
+        return 0;
+    }
+    xcb_window_t window = *(xcb_window_t *)xcb_get_property_value(reply);
+    free(reply);
+    return window;
+}
+
+
+char *
+get_window_string_property(xcb_connection_t *conn, xcb_window_t window, char *property, char *type, uint8_t empty_to_null) {
+    xcb_get_property_reply_t *reply = get_window_property_reply(conn, window, property, type);
+    if (!reply) {
+        return NULL;
+    }
     int length = xcb_get_property_value_length(reply);
     if (length == 0 && empty_to_null) {
         return NULL;
@@ -78,31 +90,37 @@ char *get_window_property(xcb_connection_t *conn, xcb_window_t window, char *pro
 }
 
 
-int main() {
+int
+main() {
     xcb_connection_t *conn = xcb_connect(NULL, NULL);
     if (xcb_connection_has_error(conn)) {
         xcb_disconnect(conn);
-        die("error opening display");
+        die("cannot connect to X server");
     }
 
-    uint32_t window = get_focused_window(conn);
+    xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
+    if (!screen) {
+        die("cannot get screen");
+    }
+
+    xcb_window_t window = get_focused_window(conn, screen->root);
     if (!window) {
         die("cannot find focused window");
     }
 
     char *name;
-    name = get_window_property(conn, window, "_NET_WM_NAME", "UTF8_STRING", 1);
+    name = get_window_string_property(conn, window, "_NET_WM_NAME", "UTF8_STRING", 1);
     if (!name) {
-        name = get_window_property(conn, window, "WM_NAME", "UTF8_STRING", 1);
+        name = get_window_string_property(conn, window, "WM_NAME", "UTF8_STRING", 1);
     }
     if (!name) {
-        name = get_window_property(conn, window, "WM_NAME", "STRING", 0);
+        name = get_window_string_property(conn, window, "WM_NAME", "STRING", 0);
     }
     if (!name) {
         name = "";
     }
 
-    char *raw_class = get_window_property(conn, window, "WM_CLASS", "STRING", 0);
+    char *raw_class = get_window_string_property(conn, window, "WM_CLASS", "STRING", 0);
     if (!raw_class) {
         die("cannot read window class");
     }
